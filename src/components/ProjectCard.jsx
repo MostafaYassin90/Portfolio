fbed38e1bbfe3cabfee99af1a7ad386006129ca5
Mostafa Yassin <mostafayassin292@gmail.com>
2025-07-27 19:42:24 +0300

    add some changes

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -7,8 +7,8 @@ const ProjectCard = ({ project }) => {
   const navigate = useNavigate();
 
   return (
-    <motion.div initial={{y: 100, opacity:0}} whileInView={{y:0, opacity: 1}} transition={{duration:0.5, delay:project.delay}}
-     key={project.id} className='rounded-md overflow-hidden border-3 border-[#13141b] shadow-gray-800 shadow-sm'>
+    <motion.div initial={{ y: 100, opacity: 0 }} whileInView={{ y: 0, opacity: 1 }} transition={{ duration: 0.5, delay: project.delay }}
+      key={project.id} className='rounded-md overflow-hidden border-3 border-[#13141b] shadow-gray-800 shadow-sm'>
 
       <div
         onClick={() => { navigate(`/projects/project/${project.id}`); scrollTo(0, 0) }}
@@ -17,7 +17,7 @@ const ProjectCard = ({ project }) => {
         <div className='absolute text-[#fff] w-full h-0 left-0 bottom-0 flex items-center justify-center bg-black/50 transition-all duration-300 group-hover:h-full'>
           <div className='flex items-center justify-center gap-3 mt-5'>
             <FaArrowLeft />
-            <p className='text-[#fff] text-2xl font-semibold'>Product Info</p>
+            <p className='text-[#fff] text-2xl font-semibold'>Project Info</p>
             <FaArrowRight />
           </div>
         </div>
