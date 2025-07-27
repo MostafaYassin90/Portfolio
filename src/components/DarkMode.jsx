7d7872d0eb5074d5e4236eeddc707645c30b4673
Mostafa Yassin <mostafayassin292@gmail.com>
2025-07-27 19:52:49 +0300

    add some changes

--- src/components/DarkMode.jsx
+++ src/components/DarkMode.jsx
@@ -3,16 +3,17 @@ import { FaMoon } from "react-icons/fa";
 import { IoSunnyOutline } from "react-icons/io5";
 
 const DarkMode = () => {
-  const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState("dark");
 
-  const toggleTheme = () => { 
+  const toggleTheme = () => {
     const mode = theme === "light" ? "dark" : "light";
     setTheme(mode)
     document.documentElement.classList.toggle("dark", mode === "dark")
     window.localStorage.setItem("theme", mode)
-  } 
+  }
 
   useEffect(() => {
+    window.localStorage.setItem("theme", localStorage.getItem("theme") ? localStorage.getItem("theme") : "dark")
     setTheme(localStorage.getItem("theme") || "light")
     document.documentElement.classList.toggle("dark", localStorage.getItem("theme") === "dark")
   }, [])
@@ -24,11 +25,11 @@ const DarkMode = () => {
         theme === "light"
           ?
           <button onClick={toggleTheme} className="w-9 h-9 rounded-full flex items-center justify-center cursor-pointer shadow-[#B923E1] shadow-md z-100 bg-[#111] dark:bg-gray-200">
-            <FaMoon className="text-xl text-white dark:text-[#111]"/>
+            <FaMoon className="text-xl text-white dark:text-[#111]" />
           </button>
           :
           <button onClick={toggleTheme} className="w-9 h-9 rounded-full flex items-center justify-center cursor-pointer shadow-[#B923E1] shadow-md z-100 bg-[#111] dark:bg-gray-200">
-            <IoSunnyOutline className="text-xl text-white dark:text-[#111]"/>
+            <IoSunnyOutline className="text-xl text-white dark:text-[#111]" />
           </button>
       }
 
